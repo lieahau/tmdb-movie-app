ff65fa5ffd53122d585d340f56572ccbbe2603a5
Budiman Putra Jaya <budimanputrajaya2@gmail.com>
2025-08-16 16:03:37 +0700

    fix reference error process.env

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = process.env.VITE_TMDB_API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY || process.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const tmdb = axios.create({
